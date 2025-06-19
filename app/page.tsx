d7796b26882cbb9fff34df9427b3d0d7e7ad3ee8
ayush24k <ayushanshu69@gmail.com>
2025-06-19 18:31:28 +0530

    env

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 
 export default function Home() {
 
-  const {sendMessage} = useSocket();
+  const { sendMessage, messages } = useSocket();
   const [message, setMessage] = useState('')
   return (
     <main className="bg-slate-900 h-screen w-screen text-white">
@@ -14,8 +14,15 @@ export default function Home() {
             <h1>All message will appear here</h1>
           </div>
           <div>
-            <input onChange={(e) => {setMessage(e.target.value)}} placeholder="messages" className="bg-slate-100 text-black rounded p-1"></input>
-            <button onClick={() => {sendMessage(message)}}>Send</button>
+            <input onChange={(e) => { setMessage(e.target.value) }} placeholder="messages" className="bg-slate-100 text-black rounded p-1"></input>
+            <button onClick={() => { sendMessage(message) }}>Send</button>
+          </div>
+          <div>
+            {messages.map((message, i) => {
+              return (
+                <li key={i}>{message}</li>
+              )
+            })}
           </div>
         </div>
       </div>
--- context/SocketProvider.tsx
+++ context/SocketProvider.tsx
@@ -7,19 +7,22 @@ interface SocketProviderProps {
 
 interface ISocketContext {
     sendMessage: (msg: string) => any;
+    messages: string[];
 }
 
 const SocketContext = React.createContext<ISocketContext | null>(null);
 
 export const useSocket = () => {
     const state = useContext(SocketContext);
-    if (!state) throw new Error (`state is undefined`);
+    if (!state) throw new Error(`state is undefined`);
 
     return state;
 };
 
-export default function SocketProvider ({children}: SocketProviderProps) {
+export default function SocketProvider({ children }: SocketProviderProps) {
     const [socket, setSocket] = useState<WebSocket | null>(null);
+    const [messages, setMessages] = useState<string[]>([]);
+
     const sendMessage: ISocketContext['sendMessage'] = useCallback((msg) => {
         console.log("send message", msg);
         if (socket) {
@@ -28,16 +31,26 @@ export default function SocketProvider ({children}: SocketProviderProps) {
     }, [socket]);
 
     useEffect(() => {
-        const _socket = new WebSocket("http://localhost:8000");
+        const _socket = new WebSocket(process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8000");
         setSocket(_socket);
+
+        _socket.onmessage = (message) => {
+            console.log('from server message recieved:', message.data);
+            setMessages((prev) => [...prev, message.data]);
+        }
+
+        _socket.onclose = () => {
+            console.log('WebSocket disconnected');
+        };
         return (() => {
             _socket.close();
             setSocket(null);
+            console.log("socket closed");
         })
     }, [])
 
     return (
-        <SocketContext.Provider value={{sendMessage}}>
+        <SocketContext.Provider value={{ sendMessage, messages }}>
             {children}
         </SocketContext.Provider>
     )
